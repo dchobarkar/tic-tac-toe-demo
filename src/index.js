128503f0c58e590edb0856c01151055d12fb97bb
Darshan Chobarkar <dchobarkar@gmail.com>
2024-01-11 11:49:52 +0530

    complete design

--- src/index.js
+++ src/index.js
@@ -62,10 +62,11 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
+
+    if (calculateWinner(squares) || squares[i]) return;
+
     squares[i] = this.state.xIsNext ? "X" : "O";
+
     this.setState({
       history: history.concat([
         {
@@ -99,11 +100,8 @@ class Game extends React.Component {
     });
 
     let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
+    if (winner) status = "Winner: " + winner;
+    else status = "Next player: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div className="game">
@@ -113,6 +111,7 @@ class Game extends React.Component {
             onClick={(i) => this.handleClick(i)}
           />
         </div>
+
         <div className="game-info">
           <div>{status}</div>
           <ol>{moves}</ol>
@@ -121,8 +120,6 @@ class Game extends React.Component {
     );
   }
 }
-
-// ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
@@ -138,11 +135,12 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
       return squares[a];
-    }
   }
+
   return null;
 }
